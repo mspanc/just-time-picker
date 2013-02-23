5ade1f55f4fd50a11a0bead4517dfc004caf67db
Marcin Lewandowski <marcin@saepia.net>
2013-02-23 13:16:14 +0100

    Renamed remaining files/directories

--- lib/just-time-picker/engine.rb
+++ lib/just-time-picker/engine.rb
@@ -2,7 +2,7 @@ module Just
   module TimePicker
     class Engine < Rails::Engine
       if Rails.version >= "3.1"
-        initializer "Just DateTime Picker precompile hook" do |app|
+        initializer "Just Time Picker precompile hook" do |app|
           app.config.assets.precompile += %w(just_time_picker/base.css)
         end
       end
